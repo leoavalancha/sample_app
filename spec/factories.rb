8122749aa38bd45259b32a7675c4946584e52a0e
Leopoldo Taravilse <leopoldo@avalancha.com>
2014-02-21 15:15:13 -0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Leopoldo Taravilse"
+    email    "leopoldo@avalancha.com"
+    password "abc123"
+    password_confirmation "abc123"
+  end
+end
