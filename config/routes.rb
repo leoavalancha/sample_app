a3f7018eba344caaf8f145bf796611aae24488d0
Leopoldo Taravilse <leopoldo@avalancha.com>
2014-02-24 13:00:51 -0300

    bla

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SampleApp::Application.routes.draw do
   resources :users
-  resources :users
   resources :sessions,   only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
   root 'static_pages#home'
